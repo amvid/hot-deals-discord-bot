47d8b9a37df142c2e9032b6aa0ed2e997e7079d4
Vadim Filipovsky <vadim.filipovsky@gmail.com>
2021-10-26 21:01:56 +0300

    feat: dockerize and nodemon

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
-const axios = require('axios');
-const Discord = require('discord.js');
-const cheerio = require('cheerio');
-require('dotenv').config();
+const axios = require("axios");
+const Discord = require("discord.js");
+const cheerio = require("cheerio");
+require("dotenv").config();
 
-
-const HOT_DEALS_URL = 'https://gg.deals';
-const HOT_DEALS_URI = '/deals/hot-new-deals/';
+const HOT_DEALS_URL = "https://gg.deals";
+const HOT_DEALS_URI = "/deals/hot-new-deals/";
 const INTERVAL_TIME = 86400000; // daily
 const BEST_DEALS_CHANNEL = process.env.CHANNEL_ID;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const bot = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log(`Bot ready as ${ bot.user.tag }`);
+bot.on("ready", () => {
+  console.log(`Bot ready as ${bot.user.tag}`);
 });
 
 bot.setInterval(async () => {
-    const response = await axios.get(HOT_DEALS_URL + HOT_DEALS_URI);
+  const response = await axios.get(HOT_DEALS_URL + HOT_DEALS_URI);
 
-    if (response.status !== 200) {
-        console.log(response.statusText);
-        return false;
+  if (response.status !== 200) {
+    console.log(response.statusText);
+    return false;
+  }
+
+  $ = cheerio.load(response.data);
+
+  $("div.game-list-item").each((i, element) => {
+    const title = $(element).find("a.title").first().text();
+
+    if (title === undefined) {
+      return;
     }
 
-    $ = cheerio.load(response.data);
+    const link = $(element).find("a.full-link").attr("href");
+    const discount = $(element).find("span.discount-badge").text();
+    const beforePrice = $(element).find("span.price-old").text();
+    const newPrice = $(element).find("span.game-price-new").text();
 
-    $('div.game-list-item').each((i, element) => {
-        const title = $(element).find('a.title').first().text();
-
-        if (title === undefined) {
-            return;
-        }
-
-        const link = $(element).find('a.full-link').attr('href');
-        const discount = $(element).find('span.discount-badge').text();
-        const beforePrice = $(element).find('span.price-old').text();
-        const newPrice = $(element).find('span.game-price-new').text();
-
-        setTimeout(() =>
-            bot.channels
-                .get(BEST_DEALS_CHANNEL)
-                .send(`${ discount } ${ title }\n${ newPrice } (${ beforePrice })\n${ HOT_DEALS_URL + link }`),
-            3000 * i
-        );
-    });
-
-
+    setTimeout(
+      () =>
+        bot.channels
+          .get(BEST_DEALS_CHANNEL)
+          .send(
+            `${discount} ${title}\n${newPrice} (${beforePrice})\n${
+              HOT_DEALS_URL + link
+            }`
+          ),
+      3000 * i
+    );
+  });
 }, INTERVAL_TIME);
 
-bot.login(BOT_TOKEN)
-    .then(() => console.log('App started...'))
-    .catch(err => console.log(err));
+bot
+  .login(BOT_TOKEN)
+  .then(() => console.log("App started..."))
+  .catch((err) => console.log(err));
